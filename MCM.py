bb99656abb56bbc9328a505f767f431aaaf3f962
CaptainMexiko <didotgwendal@gmail.com>
2018-02-07 15:58:53 +0100

    Changement de l'affichage du retour

--- MCM.py
+++ MCM.py
@@ -18,9 +18,11 @@ def clientConnect(listClient):
         print("Debut de la connexion")
         client.connect('e008m0{}.istic.univ-rennes1.fr'.format(i), username = '14002346', password='Vivelavie2*')
         stdin, stdout, stderr = client.exec_command('cd Documents/ProjetIrma')
-        print(stdin + stdout + stderr)
+        for ligne in stdout.read().splitLines():
+            print(ligne)
         stdinML, stdoutML, stderrML = client.exec_command('./MLFood.py 10')
-        print(stdinML + stdoutML + stderrML)
+        for ligne in stdoutML.read().splitLines():
+            print(ligne)
         i += 1
         client.close()
         pass
