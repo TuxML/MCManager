d1634bd0fab7b51350cafe0d667944c19ff5dda9
CaptainMexiko <didotgwendal@gmail.com>
2018-02-14 16:57:40 +0100

    Test pour affichage continu

--- MCM.py
+++ MCM.py
@@ -13,6 +13,15 @@ def makeSSHClient(number):
     pass
 
 
+def line_buffered(f):
+    line_buf = ""
+    while not f.channel.exit_status_ready():
+        line_buf += f.read(1)
+        if line_buf.endswith('\n'):
+            yield line_buf
+            line_buf = ''
+
+
 def clientConnect(listClient):
     i = 1
     for client in listClient:
@@ -20,7 +29,9 @@ def clientConnect(listClient):
         print("{}".format(i))
         client.connect('e212m0{}.istic.univ-rennes1.fr'.format(i), username = '14002346', password='Vivelavie2*')
         stdin, stdout, stderr = client.exec_command('cd /private/student/6/46/14002346/Documents/ProjetIrma; ./MLfood.py 10')
-        print(stderr.read())
+        for ligne in line_buffered(stdout):
+            print(ligne)
+            pass
         i += 1
         client.close()
         pass
