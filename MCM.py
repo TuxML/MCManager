121b6dbc1b75a9482f03a291f845966f819b9bed
Didot Gwendal <dgwendal@dn028247.etu.univ-rennes1.fr>
2018-03-14 17:57:06 +0100

    debug

--- MCM.py
+++ MCM.py
@@ -35,7 +35,7 @@ def file_len(fname):
 
 def clientConnect(client, clientName, username, password):
     print("Debut de la connexion")
-    print(clientName)
+    print("{}.istic.univ-rennes1.fr".format(clientName))
     client.connect("{}.istic.univ-rennes1.fr".format(clientName), username =  username, password = password)
     stdin, stdout, stderr = client.exec_command('cd /private/student/6/46/14002346/Documents/ProjetIrma; ./MLfood.py 1')
     client.close()
