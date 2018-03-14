6f18a42a747a9bb4cdde76c0ab3de71d5d2ec069
Didot Gwendal <dgwendal@localhost.localdomain>
2018-03-14 14:34:24 +0100

    Système de fork mis en place

--- MCM.py
+++ MCM.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
 
 import paramiko
+import os
+import getpass
 from multiprocessing import Pool
 from sys import argv
 from contextlib import closing
@@ -24,29 +26,38 @@ def line_buffered(f):
             line_buf = ''
 
 
-def clientConnect(client):
-    i = 2
-    #for client in listClient:
-    print("Debut de la connexion")
-    print("{}".format(i))
-    username = input("User name.")
-    password = input("Password.")
-    client.connect('e008m0{}.istic.univ-rennes1.fr'.format(i), username =  username, password = password)
-    stdin, stdout, stderr = client.exec_command('cd /private/student/6/46/14002346/Documents/ProjetIrma; ./MLfood.py 1')
-
-    for ligne in line_buffered(stdout):
-        print(ligne)
+def clientConnect(listClient, idPC):
+    username = input("Username:")
+    password = getpass.getpass("Password:")
+    for client in listClient:
+        print("Debut de la connexion")
+        print("{}".format(idPC))
+        client.connect('e212m0{}.istic.univ-rennes1.fr'.format(idPC), username =  username, password = password)
+        stdin, stdout, stderr = client.exec_command('cd /private/student/6/46/14002346/Documents/ProjetIrma; ./MLfood.py 1')
+        client.close()
         pass
-    i += 1
-    client.close()
     pass
-    #pass
 
 
 if __name__ == '__main__':
+    forks = 2
+    idPC = 1
     listClient = makeSSHClient(int(argv[1]))
-    with closing(Pool(2)) as worker:
-        worker.map(clientConnect, listClient)
-        worker.terminate()
+    for idFork in range(forks):
+        try:
+            pid = os.fork()
+        except OSError:
+            sys.stderr.write("Could not create the child process")
+            continue
+        if pid == 0:
+            clientConnect(listClient, idPC)
+            pass
+        else:
+            print("In the parent process after forking children number {}".format(idFork))
+        idPC += 1
         pass
-    clientConnect(listClient)
+    print("End of the forking")
+    for i in range(forks):
+        finished = os.waitpid(0, 0)
+        print(finished)
+        pass
