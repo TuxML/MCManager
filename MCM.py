e238a334792bec6609a27da88667d23e0e6f4a71
CaptainMexiko <didotgwendal@gmail.com>
2018-03-28 16:48:59 +0200

    Test fix

--- MCM.py
+++ MCM.py
@@ -44,7 +44,7 @@ def clientConnect(client, clientName, username, password):
 
 if __name__ == '__main__':
     listOrdi = open('BaseDeDataTest.txt', 'r')
-    listClient = makeSSHClient(file_len("BaseDeData.txt"))
+    listClient = makeSSHClient(10)
     username = raw_input("Username:")
     password = getpass.getpass("Password:")
     for client in listClient:
