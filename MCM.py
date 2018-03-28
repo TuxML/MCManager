d537c14af7f8f26aa285cf77d45e056ba0d4e6a3
CaptainMexiko <didotgwendal@gmail.com>
2018-03-28 17:18:40 +0200

    Changement du calcul nombre pc

--- MCM.py
+++ MCM.py
@@ -44,7 +44,11 @@ def clientConnect(client, clientName, username, password):
 
 if __name__ == '__main__':
     listOrdi = open('BaseDeDataTest.txt', 'r')
-    listClient = makeSSHClient(10)
+    number_ordi = 0
+    for line in listOrdi:
+        number_ordi += 1
+    listOrdi.seek(0)
+    listClient = makeSSHClient(number_ordi)
     username = raw_input("Username:")
     password = getpass.getpass("Password:")
     for client in listClient:
